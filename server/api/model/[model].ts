c5c9a8ac8840c452ece09f44e22d776d4bedb090
Pascal Martineau <pascal@lewebsimple.ca>
2023-07-10 22:36:58 -0400

    wip: @zenstack/server server endpoint

---
+++ server/api/model/[model].ts
@@ -0,0 +1,6 @@
+import { ZenStackMiddleware } from "@zenstackhq/server/express";
+
+export default defineEventHandler(async (event) => {
+  // TODO: Handle event with ZenStackMiddleware (i.e. express middleware)
+  return {};
+});
