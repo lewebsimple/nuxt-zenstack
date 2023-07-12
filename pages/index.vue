84494eac0baeac44f168a2622fa44f1c2a3c9201
Pascal Martineau <pascal@lewebsimple.ca>
2023-07-12 09:21:32 -0400

    minor: remove stuff from front page

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,9 @@
 <script setup lang="ts">
 useHead({ title: "Home" });
-const { $trpc } = useNuxtApp();
-const { data: posts } = await $trpc.crud.post.findMany.useQuery({});
-const { data: version } = await $trpc.info.version.useQuery();
 </script>
 
 <template>
   <div id="page-home">
     <h1>Nuxt / ZenStack</h1>
-    <pre>{{ posts }}</pre>
-    <div>{{ version }}</div>
   </div>
 </template>
