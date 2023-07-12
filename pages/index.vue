0a77411386c9639728de250c15e6d2232563c0d1
Pascal Martineau <pascal@lewebsimple.ca>
2023-07-12 09:24:02 -0400

    minor: login / logout button on home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 useHead({ title: "Home" });
+const { isAuthenticated } = useAuth();
 </script>
 
 <template>
-  <div id="page-home">
-    <h1>Nuxt / ZenStack</h1>
+  <div id="page-home" class="space-y-6">
+    <h1 class="font-bold text-4xl">Nuxt / ZenStack</h1>
+    <UButton v-if="isAuthenticated" to="/logout">Logout</UButton>
+    <UButton v-else to="/login">Login</UButton>
   </div>
 </template>
